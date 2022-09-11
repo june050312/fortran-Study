e2589adfd65920da993eb50179122a25d3fa0668
june050312 <june050312@gmail.com>
2022-09-11 21:37:34 +0900

    first commit

---
+++ 2/example.f
@@ -0,0 +1,8 @@
+* Programed by HyungJun
+c Programed in 11st of Sep, 2022
+	 program hi
+		write (*, *) 'Hi!'
+		write (*, *) 'HyungJun 0123456789'
+		circle = 3.14159265358972
+	 +			 * r * r
+	  end
